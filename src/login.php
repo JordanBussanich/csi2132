39608aa547513427fade1edcb690a3309b776a39
Jordan Bussanich <jordan.bussanich@gmail.com>
2013-04-06 15:28:03 -0400

    Finished registration.

--- src/login.php
+++ src/login.php
@@ -7,7 +7,7 @@
 //start the users login session
 session_start();
 
-//get the enetered login credentials from the form on index.php
+// Get the email and password (from index.php)
 $email = $_POST['email'];
 $password = $_POST['password'];
 
@@ -18,11 +18,11 @@ if($email&&$password)
 	include ("config.php");
 
 	// Attempt to connect
-	$connect = pg_connect($connection) or die("Could not connect to SQL server");
+	$connect = pg_connect($connection) or die("Error: Could not connect to SQL server. Please contact <a href='mailto://jordan.bussanich@gmail.com'>Jordan Bussanich</a> to report this error.");
 
 	// Attempt to login
-	$hashpass = md5($password);		// May need to change this to not use a hash
-	$query = "SELECT m.MemberNumber FROM Member m WHERE m.email = '$email' AND m.password = '$hashpass'";
+	//$hashpass = md5($password);		// May need to change this to not use a hash
+	$query = "SELECT m.MemberNumber FROM Member m WHERE m.email = '$email' AND m.password = '$password'";
 	
 	$result = pg_query($query);
 	
--- src/register.php
+++ src/register.php
@@ -2,89 +2,110 @@
 /*
  *	Registers a new user
  */
-
-	echo '<h1>Register:</h1>';
 	
-	//include the nessisary hosting SQL info
-	include ("config.php");
-	//connect to the peirce.me server, and connect to the a8341597_AVA database which contains the user credentials
-	if(mysql_connect($host, $user, $pass) && mysql_select_db($db))
-	{
-		//true if the user has pressed submit
-		$submit = @ $_POST['submit'];
-		//set variable from the form submited
-		$firstname = strip_tags( @$_POST['firstname']);
-		$lastname = strip_tags( @$_POST['lastname']);
-		$email = strip_tags( @$_POST['email']);
-		$password = strip_tags( @ $_POST['password']);
-		$repeatpassword = strip_tags( @ $_POST['repeatpassword']);
-		
-		//when submited
-		if($submit){
-			
-			//connect to the peirce.me server, and connect to the a8341597_AVA database which contains the user credentials
-			$connect = mysql_connect($host, $user, $pass);
-			mysql_select_db($db); //select database
-			
-			//query the server for the usernames 
-			$namecheck = mysql_query("SELECT username FROM users WHERE username='$username'"); 
-			$count = mysql_num_rows($namecheck);
-			//if no usernames kill the page
-			if($count!=0){
-				die("Username already taken");
-			}
-			//if all the form fields are filled out
-			if($username&&$fullname&&$password&&$repeatpassword&&$email){
-				//if both enetered passwords match
-				if($password==$repeatpassword){
-					//if both the username or full name are greater than 25 characters, error
-					if(strlen($username)>25 || strlen($fullname)>25){
-						echo 'max limit for username and fullname is 25 characters';
-					}
-					else{
-						//if the password is greater than 25 or less than 6, error
-						if(strlen($password)>25|| strlen($password)<6){
-							echo 'Passord must be between 6 and 25 characters ';
-						}
-						
-						//if not of the above error conditions have been met, allow the user to be registered
-						else{
-							//Hash password
-							$password = md5($password);
-							$repeatpassword = md5($repeatpassword);
-							
-							$queryreg = "INSERT INTO `users` VALUES ('','$fullname','$email','$username','$password','$date','')";
-							if(mysql_query($queryreg)){
-								//when the user has been regitered link them back to the login index.php page
-								die("you have been registered, <a href= 'index.php'>Return to login page</a>");
-							}
-							
-						}
-						
-					}
-				
-				}
-				//tell the user their passwords dont match
-				else{
-					echo 'Error: Passwords do not match.';
-				}
-				
-			}
-			//tell the user they have left fields blank
-			else
-				 echo 'Please complete all fields';
-		}
-	}
-	//if the page could not connect to the server
-	else{
-		echo 'Could not connect';
-	}
+// Get the SQL server variables
+include ("config.php");
 
-//code for the registration form	
+// Should be true if the user clicked 'Register'
+$submit = @ $_POST['submit'];
+
+// Get the fields
+
+// User info
+$firstname = strip_tags( @$_POST['firstname']);
+$lastname = strip_tags( @$_POST['lastname']);
+$email = strip_tags( @$_POST['email']);
+$password = strip_tags( @ $_POST['password']);
+$repeat = strip_tags( @ $_POST['repeat']);
+
+// Card info
+$card = strip_tags( @$_POST['card']);
+$cardnum = strip_tags( @$_POST['cardnum']);
+$expdate = strip_tags( @ $_POST['expdate']);
+$namecard = strip_tags( @ $_POST['namecard']);
+
+// Billing info
+$address = strip_tags( @$_POST['address']);
+$city = strip_tags( @$_POST['city']);
+$postcode = strip_tags( @ $_POST['postcode']);
+$phone = strip_tags( @ $_POST['phone']);
+
+// Shipping info
+$saddress = strip_tags( @$_POST['saddress']);
+$scity = strip_tags( @$_POST['scity']);
+$spostcode = strip_tags( @ $_POST['spostcode']);
+$sphone = strip_tags( @ $_POST['sphone']);
+
+//when submited
+if($submit)
+{
+	
+	// Attempt to connect
+	$connect = pg_connect($connection) or die("Error: Could not connect to SQL server. Please contact <a href='mailto://jordan.bussanich@gmail.com'>Jordan Bussanich</a> to report this error.");
+	
+	// Make sure an the given email doesn't already have an account
+	$namecheck = pg_query("SELECT email FROM Member WHERE email='$email'"); 
+	$count = pg_num_rows($namecheck);
+	
+	// If taken, kill the page
+	if($count!=0)
+	{
+		die("An account already exists for that email.<br/><br/><a href='http://jordanb.no-ip.org/csi2132'>Return Home</a>");
+	}
+	
+	// Ensure all of the fields are filled out
+	if($firstname && $lastname && $email && $password && $repeat && $card && $cardnum && $expdate && $namecard && $address && $city && $postcode && $phone && $saddress && $scity && $spostcode && $sphone)
+	{
+		// Ensure the passwords match
+		if($password==$repeat)
+		{
+			// Ensure the password is > 6 chars
+			if(strlen($password) < 6)
+			{
+				echo 'Error: Password must be at least 6 characters.';
+			}
+			// We got this far, register the user
+			else
+			{
+				//Hash password (typically we'd do this, but we're not going to for purpose of this project)
+				//$password = md5($password);
+				//$repeat = md5($repeat);
+				
+				// Register the member and the card first (we need these to register the BillingInfo, BillingAddress and ShippingAddress)
+				$regmember = "INSERT INTO `Member` VALUES ('', '$firstname', '$lastname', '$email', '$password')";
+				$regcard = "INSERT INTO `Card` VALUES ('$cardnum', '$expdate', '$card')";
+				
+				if(pg_query($regmember) && pg_query($regcard))
+				{
+					// Get the member number
+					$memnum = "SELECT m.MemberNumber FROM Member m WHERE m.email = '$email' AND m.password = '$password'";
+					
+					// Now register BillingInfo, BillingAddress and ShippingAddress
+					$regbillinfo = "INSERT INTO `BillingInformation` VALUES ('$memnum', '$cardnum', 'credit')";
+					$regbilladdr = "INSERT INTO `BillingAddress` VALUES ('', '$memnum', '$address', '', '$city', '$phone', '$postcode)";
+					$regshipaddr = "INSERT INTO `ShippingAddress` VALUES ('', '$memnum', '$saddress', '', '$scity', '$sphone', '$spostcode)";
+					
+					if(pg_query($regbillinfo) && pg_query($regbilladdr) && pg_query($regshipaddr))
+					{
+						die("Registration successful.<br/><a href= 'index.php'>Return to login page</a>");
+					}
+				}
+			}
+		}
+		else
+		{
+			echo 'Error: Passwords do not match.';
+		}
+		
+	}
+	else
+	{
+		 echo 'Error: All fields must be completed.';
+	}
+}
 ?>
 <form action="register.php" method="post">
-	<br />
-	<h1>Register</h1>
+	<h1>NetFilms Registration</h1>
 	<h2>User Information</h2>
 	<table border="1" bordercolor="#000000" style="background-color:#FFFFFF" width="40%" cellpadding="1" cellspacing="3">
 	<tr>
@@ -100,7 +121,7 @@
 		<td><input type="email" name="email" size="40%"></td>
 	</tr>
 	<tr>
-		<td>Password</td>
+		<td>Password <i>Must be greater than 6 characters</i></td>
 		<td><input type="password" name="password" size="40%"></td>
 	</tr>
 	<tr>
@@ -117,7 +138,7 @@
 	</tr>
 	<tr>
 		<td>Card Number</td>
-		<td><input type="text" name="number" size="40%"></td>
+		<td><input type="text" name="cardnum" size="40%"></td>
 	</tr>
 	<tr>
 		<td>Expiration Date (MM/YY)</td>
@@ -168,56 +189,8 @@
 		<td><input type="text" name="sphone" size="40%"></td>
 	</tr>
 	</table>
-	
-</form>
-
-	
-	
-	
-	<table>
-		<tr>
-			<td>
-			First Name:
-			</td>
-			<td>
-			<input type="text" name="firstname" value="<?php echo @$firstname; ?>">
-			</td>
-		</tr>
-		<tr>
-			<td>
-			Last Name:
-			</td>
-			<td>
-			<input type="text" name="lastname" value="<?php echo @$lastname; ?>">
-			</td>
-		</tr>
-		<tr>
-			<td>
-			Email Address:
-			</td>
-			<td>
-			<input type="text" name="email" value="<?php echo @$email; ?>">
-			</td>
-		</tr>
-		<tr>
-			<td>
-			Password:
-			</td>
-			<td>
-			<input type="password" name="password" >
-			</td>
-		</tr>
-		<tr>
-			<td>
-			Repeat password:
-			</td>
-			<td>
-			<input type="password" name="repeatpassword" >
-			</td>
-		</tr>
-	</table>
-	<p>
+	<br/>
+	<p><i>Please ensure that all fields are completed before clicking 'Register'</i></p>
+	<br/>
 	<input type="submit" name="submit" value="Register">
 </form>
-
-	
